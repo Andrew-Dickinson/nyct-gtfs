820a6ee10a828504c95299fe50ac31c1d56b76f2
Andrew Dickinson <andrew.dickinson.0216@gmail.com>
2021-12-11 14:58:44 -0500

    Cache data at the wrapper layer

--- nyct_gtfs/cpp_parser_wrapper.py
+++ nyct_gtfs/cpp_parser_wrapper.py
@@ -1,5 +1,6 @@
 import nyct_gtfs_cpp
 
+
 class RepeatedFieldIterator:
     def __init__(self, repeated_field):
         self.idx = 0
@@ -24,6 +25,7 @@ class RepeatedField:
         self._attr_type = attr_type
         self._ext = ext
         self._len = nyct_gtfs_cpp.get_size(self._message_ptr, self._field_name)
+        self._cached_items = [None for i in range(self._len)]
 
     def __len__(self):
         return self._len
@@ -34,6 +36,9 @@ class RepeatedField:
     def __getitem__(self, index):
         if index >= self._len:
             raise IndexError("RepeatedField index out of range")
+
+        if self._cached_items[index] is not None:
+            return self._cached_items[index]
 
         func = None
         if self._attr_type.name in {"UINT64", "UINT32", "BOOL", "ENUM"}:
@@ -56,26 +61,35 @@ class RepeatedField:
             raw_result = func(self._message_ptr, "", index, self._ext)
 
         if self._attr_type.name == "MESSAGE":
-            return ProxyMessage(raw_result)
-        return raw_result
+            result = ProxyMessage(raw_result)
+        else:
+            result = raw_result
+        self._cached_items[index] = result
+        return result
 
 
 class ProxyMessage:
     def __init__(self, message_ptr, is_ext=False):
         self._message_ptr = message_ptr
         self._is_ext = is_ext
+        self._cached_attrs = {}
+        self._cached_exts = {}
 
     def __getitem__(self, ext):
         ext_id = ext.number
         if not self._is_ext:
             raise ValueError("Bracket syntax is only available on messages if they are tagged as Extension objects")
 
+        if ext_id in self._cached_exts:
+            return self._cached_exts[ext_id]
+
         attr_type = nyct_gtfs_cpp.get_type(self._message_ptr, "", ext_id)
         if attr_type.value == 0:
             raise AttributeError(f"No extension {ext_id}")
 
         if nyct_gtfs_cpp.is_repeated(self._message_ptr, "", ext_id):
-            return RepeatedField(self._message_ptr, "", attr_type, ext_id)
+            self._cached_exts[ext_id] = RepeatedField(self._message_ptr, "", attr_type, ext_id)
+            return self._cached_exts[ext_id]
 
         func = None
         if attr_type.name in {"UINT64", "UINT32", "BOOL", "ENUM"}:
@@ -94,17 +108,24 @@ class ProxyMessage:
 
         raw_result = func(self._message_ptr, "", ext_id)
         if attr_type.name == "MESSAGE":
-            return ProxyMessage(raw_result)
+            result = ProxyMessage(raw_result)
         else:
-            return raw_result
+            result = raw_result
+
+        self._cached_exts[ext_id] = result
+        return result
 
     def __getattr__(self, name):
+        if name in self._cached_attrs:
+            return self._cached_attrs[name]
+
         attr_type = nyct_gtfs_cpp.get_type(self._message_ptr, name)
         if attr_type.value == 0:
             raise AttributeError(f"No attribute {name}")
 
         if nyct_gtfs_cpp.is_repeated(self._message_ptr, name):
-            return RepeatedField(self._message_ptr, name, attr_type)
+            self._cached_attrs[name] = RepeatedField(self._message_ptr, name, attr_type)
+            return self._cached_attrs[name]
 
         func = None
         if attr_type.name in {"UINT64", "UINT32", "BOOL", "ENUM"}:
@@ -123,9 +144,12 @@ class ProxyMessage:
 
         raw_result = func(self._message_ptr, name)
         if attr_type.name == "MESSAGE":
-            return ProxyMessage(raw_result)
+            result = ProxyMessage(raw_result)
         else:
-            return raw_result
+            result = raw_result
+
+        self._cached_attrs[name] = result
+        return result
 
     def HasField(self, name):
         return nyct_gtfs_cpp.has_field(self._message_ptr, name)
